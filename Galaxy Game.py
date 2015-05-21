c214384438f6764fd0f2fcc2d7d0baf3d7a56c46
Mustafa Haydar Bayzan <mustafabayzan@gmail.com>
2015-05-21 17:27:50 +0300

    Improvement on Game

--- Galaxy Game.py
+++ Galaxy Game.py
@@ -1,4 +1,6 @@
 from sys import exit
+import time
+import psutil
 from PIL import Image
 	
 def blue_galaxy():
@@ -6,11 +8,50 @@ def blue_galaxy():
 	img = Image.open('C:/Users/Macroice Deviance/Desktop/Galaxy Game/01 Blue Galaxy.jpg')
 	img.show()
 	
+	
+	time.sleep(5)
+	
+	for proc in psutil.process_iter():
+		if proc.name == "display":
+			proc.kill()
+	
+	print "Here, you can ride the wind to travel stars.\n\n"
+	time.sleep(1)
+	print "There 2 regions in stars as East and West."
+	print "West is cold. East is hot."
+	print "Do you wanna see the stars?\n\n"
+	next = raw_input("> ")
+	if next == "yes":
+		print "Here you have!"
+		img = Image.open('C:/Users/Macroice Deviance/Desktop/Galaxy Game/03 Blue Stars.jpeg')
+		img.show()
+		print "Now do you wanna go to Red Galaxy?"
+		nextie = raw_input("> ")
+		if nextie == "yes":
+			print "Then hold on tight!"
+			red_galaxy()
+		else:
+			dead("Okay, as you wish..")
+	else:
+		dead("No stars for you..")
+	
 def red_galaxy():
 	print "Come in, little boy welcome."
 	img = Image.open('C:/Users/Macroice Deviance/Desktop/Galaxy Game/02 Red Galaxy.jpg')
 	img.show()
 	
+	print "Here, you can use explosions to reach other planets.\n\n"
+	print "There 2 areas in planets as North and South"
+	print "North is bright. South is dark"
+	print "Do you wanna see the planets?"
+	next = raw_input("> ")
+	if next == "yes":
+		print "Right in front of you."
+		img = Image.open('C:/Users/Macroice Deviance/Desktop/Galaxy Game/04 Red Planets.jpg')
+		img.show()
+	else:
+		dead("No planets for you..")
+		
 def dead(why):
 	print why, "Good Job!"
 	exit(0)
